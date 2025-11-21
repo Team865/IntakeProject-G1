9ebcbac76f2d17de7a1e7fb8d8065608707955ec
ZSmith204 <zack.a.w.smith@gmail.com>
2025-11-21 14:37:55 -0500

    added akit stuf and amps to RollerIO.java

--- src/main/java/frc/robot/subsystems/intake/RollersIO.java
+++ src/main/java/frc/robot/subsystems/intake/RollersIO.java
@@ -1,16 +1,20 @@
 package frc.robot.subsystems.intake;
-interface RollersIO {
+public interface RollersIO {
+
 	@AutoLog
-	pubic class RollerIoInputs{
+	pubic class RollerIOInputs{
 		boolean conected = false;
 		float curentAmps = 0.0;
 		float currentVolts = 0.0;
 		float positioRads = 0.0;
 		float rotSpeedRadsSeconds = 0.0;
-	
 	}
 
+	private void updateInputs(RollerIOInputsAutoLogged inputs)();
+
 	private static void setVolts(int volts);
+
+	private void setAmps(int amps);
 	
 	private static void intake();
 	
