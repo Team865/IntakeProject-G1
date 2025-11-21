d8ed3a5264d8f78e75688e09c89916f7edeeb2d4
ZSmith204 <zack.a.w.smith@gmail.com>
2025-11-21 13:18:23 -0500

    created inputs with @AutoLog

--- src/main/java/frc/robot/subsystems/intake/RollersIO.java
+++ src/main/java/frc/robot/subsystems/intake/RollersIO.java
@@ -1,6 +1,19 @@
 package frc.robot.subsystems.intake;
 interface RollersIO {
+	@AutoLog
+	pubic class RollerIoInputs{
+		boolean conected = false;
+		float curentAmps = 0.0;
+		float currentVolts = 0.0;
+		float positioRads = 0.0;
+		float rotSpeedRadsSeconds = 0.0;
+	
+	}
+
 	private static void setVolts(int volts);
+	
 	private static void intake();
+	
 	private static void outtake();
 }
+
