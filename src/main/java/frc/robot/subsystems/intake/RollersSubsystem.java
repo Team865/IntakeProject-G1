77b81c771b6d9494a3882cbed877cb260b58548e
terminalcrazy1 <liam.jbm2020@gmail.com>
2025-11-23 17:01:51 -0500

    more lint errors

--- src/main/java/frc/robot/subsystems/intake/RollersSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/RollersSubsystem.java
@@ -1,45 +1,41 @@
 package frc.robot.subsystems.intake;
-<<<<<<< HEAD
-public class RollersSubsystem extends SubsystemBase {
-	private boolean isIntakeEnabled = false;
-	private boolean isOuttakeEnabled = false;
-	private ObjectDetectionIO io;
 
-	Trigger xTrigger = operatorController.x();
-	Trigger yTrigger = operatorController.y();
-
-	public RollersSubsystem(RollersIO io){
-		
-	}
-	
-	private Command intake() {
-		return runOnce(() -> {isIntakeEnabled = true; isOuttakeEnabled = false; setVolts(12);});
-	}
-	private Command outtake() {
-		return runOnce(() -> {isOuttakeEnabled = true; isIntakeEnabled = false; setVolts(-12);});
-	}
-
-	@Override
-	private void periodic{
-		if (io.distanceMM < 50 && isIntakeEnabled) {
-			isIntakeEnable = false;
-			setVolts(0);
-		} else if (io.distanceMM < 50 && isOuttakeEnabled) {
-			isOuttakeEnable = false;
-			setVolts(0);
-		}
-	}	
-=======
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class IntakeSubsystem extends SubsystemBase {
-  public IntakeSubsystem() {
+public class RollersSubsystem extends SubsystemBase {
+    private boolean isIntakeEnabled = false;
+    private boolean isOuttakeEnabled = false;
+    private ObjectDetectionIO io;
 
-  }
+    Trigger xTrigger = operatorController.x();
+    Trigger yTrigger = operatorController.y();
 
-  @Override
-  public void periodic() {
-    
-  }
->>>>>>> 9256733 (lol everything else)
+    public RollersSubsystem(RollersIO io) {}
+
+    private Command intake() {
+        return runOnce(() -> {
+            isIntakeEnabled = true;
+            isOuttakeEnabled = false;
+            setVolts(12);
+        });
+    }
+
+    private Command outtake() {
+        return runOnce(() -> {
+            isOuttakeEnabled = true;
+            isIntakeEnabled = false;
+            setVolts(-12);
+        });
+    }
+
+    @Override
+    private void periodic() {
+        if (io.distanceMM < 50 && isIntakeEnabled) {
+            isIntakeEnable = false;
+            setVolts(0);
+        } else if (io.distanceMM < 50 && isOuttakeEnabled) {
+            isOuttakeEnable = false;
+            setVolts(0);
+        }
+    }
 }
