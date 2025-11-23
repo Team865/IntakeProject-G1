64c54833d35a50ebf94b59c25adc115c34854bbd
Zachary Smith <zsmith204@zsmith-pc.zsmith204-home>
2025-11-23 17:42:09 -0500

    fix mere comit that somehow ended up on main

--- src/main/java/frc/robot/subsystems/intake/RollersIOReal.java
+++ src/main/java/frc/robot/subsystems/intake/RollersIOReal.java
@@ -4,11 +4,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 class RollersIOReal implements RollersIO {
 
-<<<<<<< HEAD
     private final TalonFX rollerMotor = new TalonFX(Constants.RollerCANID);
-=======
-	private final TalonFX rollerMotor = new TalonFX(Constants.RollerCANID);
->>>>>>> bba0f91 (fixed typo)
 
     @Override
     private void updateInputs() {}
