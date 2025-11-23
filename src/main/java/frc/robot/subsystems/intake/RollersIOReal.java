f6e523f26c738e452c5dda17e39b1989a8f193d4
Zachary Smith <zsmith204@zsmith-pc.zsmith204-home>
2025-11-23 17:38:14 -0500

    I have no idea what I changed

--- src/main/java/frc/robot/subsystems/intake/RollersIOReal.java
+++ src/main/java/frc/robot/subsystems/intake/RollersIOReal.java
@@ -4,7 +4,11 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 class RollersIOReal implements RollersIO {
 
+<<<<<<< HEAD
     private final TalonFX rollerMotor = new TalonFX(Constants.RollerCANID);
+=======
+	private final TalonFX rollerMotor = new TalonFX(Constants.RollerCANID);
+>>>>>>> bba0f91 (fixed typo)
 
     @Override
     private void updateInputs() {}
