1c6baefb0f4e9ff6addd5bfdc6dd7e0c001d0148
terminalcrazy1 <liam.jbm2020@gmail.com>
2025-11-23 17:39:13 -0500

    unstaged changes

--- src/main/java/frc/robot/subsystems/intake/RollersSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/RollersSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class RollersSubsystem extends SubsystemBase {
