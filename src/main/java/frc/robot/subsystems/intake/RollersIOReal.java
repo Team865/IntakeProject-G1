7b90a1e371a9a7f27a7facb78bc6afe03b3a6ad4
terminalcrazy1 <liam.jbm2020@gmail.com>
2025-11-23 17:32:49 -0500

    fixed various lint errors

--- src/main/java/frc/robot/subsystems/intake/RollersIOReal.java
+++ src/main/java/frc/robot/subsystems/intake/RollersIOReal.java
@@ -1,17 +1,14 @@
 package frc.robot.subsystems.intake;
 
-import com.ctre.phoenix6.hardware.TalonFX; 
+import com.ctre.phoenix6.hardware.TalonFX;
 
 class RollersIOReal implements RollersIO {
 
-	private final TalonFX rollerMotor = new TalonFX(Constants.RollerCANID);
+    private final TalonFX rollerMotor = new TalonFX(Constants.RollerCANID);
 
-	@Override
-	private void updateInputs(RollersIOInputsAutoLogged) {
-		
-	}
-	@Override
-	private void setVolts(int volts) {
-		
-	}
+    @Override
+    private void updateInputs() {}
+
+    @Override
+    private void setVolts(int volts) {}
 }
