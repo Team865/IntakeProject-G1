2e6d33906ae9e73a1efd919a3fa3d03cd59c80f1
terminalcrazy1 <liam.jbm2020@gmail.com>
2025-11-24 15:53:37 -0500

    stuff

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -3,15 +3,11 @@ package frc.robot.subsystems.intake;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class IntakeSubsystem extends SubsystemBase {
     private boolean isIntakeEnabled = false;
     private boolean isOuttakeEnabled = false;
     private ObjectDetectionIO io;
-
-    Trigger xTrigger = operatorController.x();
-    Trigger yTrigger = operatorController.y();
 
     IntakeSubsystem(IntakeIO io) {}
 
