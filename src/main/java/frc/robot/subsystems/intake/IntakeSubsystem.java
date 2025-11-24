c829b852e69f043305f77c9dbf5184c7b038dee3
ZSmith204 <zack.a.w.smith@gmail.com>
2025-11-24 16:06:50 -0500

    wip

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -7,15 +7,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class IntakeSubsystem extends SubsystemBase {
     private boolean isIntakeEnabled = false;
     private boolean isOuttakeEnabled = false;
-    private ObjectDetectionIO io;
+    private ObjectDetectionIO objectIO;
+    private IntakeIO io;
 
-    IntakeSubsystem(IntakeIO io) {}
+    IntakeSubsystem(IntakeIO io) {
+    	this.io = io;
+    }
 
     public Command intake() {
         return runOnce(() -> {
             isIntakeEnabled = true;
             isOuttakeEnabled = false;
-            setVolts(12);
+            i9o.setVolts(12);
         });
     }
 
@@ -23,7 +26,7 @@ public class IntakeSubsystem extends SubsystemBase {
         return runOnce(() -> {
             isOuttakeEnabled = true;
             isIntakeEnabled = false;
-            setVolts(-12);
+            io.setVolts(-12);
         });
     }
 
@@ -31,10 +34,10 @@ public class IntakeSubsystem extends SubsystemBase {
     private void periodic() {
         if (io.distanceMM < 50 && isIntakeEnabled) {
             isIntakeEnabled = false;
-            setVolts(0);
+            io.setVolts(0);
         } else if (io.distanceMM < 50 && isOuttakeEnabled) {
             isOuttakeEnabled = false;
-            setVolts(0);
+            io.setVolts(0);
         }
     }
 }
