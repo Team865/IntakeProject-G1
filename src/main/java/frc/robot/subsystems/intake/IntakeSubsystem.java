adf6146f4c95e6abdb650df36394b96eac8f1109
terminalcrazy1 <liam.jbm2020@gmail.com>
2025-11-26 15:50:30 -0500

    r

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -2,29 +2,38 @@ package frc.robot.subsystems.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.Mode;
 
 public class IntakeSubsystem extends SubsystemBase {
     private boolean isIntakeEnabled = false;
     private boolean isOuttakeEnabled = false;
     private ObjectDetectionIOInputsAutoLogged objectIO = new ObjectDetectionIOInputsAutoLogged();
-    private IntakeIO io;
+    private IntakeIO intakeIO;
 
     public IntakeSubsystem() {
+        if (Constants.currentMode == Mode.SIM) {
+            intakeIO = new IntakeIOSim();
+        } else if (Constants.currentMode == Mode.REAL){
+            intakeIO = new IntakeIOReal();
+        }
     }
 
     public Command intake() {
+        System.out.println("Intaking!");
         return runOnce(() -> {
             isIntakeEnabled = true;
             isOuttakeEnabled = false;
-            io.setVolts(12);
+            intakeIO.setVolts(12);
         });
     }
 
     public Command outtake() {
+        System.out.println("Outtaking!");
         return runOnce(() -> {
             isOuttakeEnabled = true;
             isIntakeEnabled = false;
-            io.setVolts(-12);
+            intakeIO.setVolts(-12);
         });
     }
 
@@ -32,10 +41,10 @@ public class IntakeSubsystem extends SubsystemBase {
     public void periodic() {
         if (objectIO.distanceMM < 50 && isIntakeEnabled) {
             isIntakeEnabled = false;
-            io.setVolts(0);
+            intakeIO.setVolts(0);
         } else if (objectIO.distanceMM < 50 && isOuttakeEnabled) {
             isOuttakeEnabled = false;
-            io.setVolts(0);
+            intakeIO.setVolts(0);
         }
     }
 }
