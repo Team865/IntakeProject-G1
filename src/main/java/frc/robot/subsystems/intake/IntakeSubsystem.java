a4a78591c2767f1988f423bad44488d268ea20eb
ZSmith204 <zack.a.w.smith@gmail.com>
2025-11-26 21:06:50 -0500

    nothing compiles but im out of time so I needed somewhere to put this

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -1,9 +1,14 @@
 package frc.robot.subsystems.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.Mode;
+import java.util.function.BooleanSupplier;
+
+private final Trigger isIntakeComplete = new Trigger(BooleanSupplier doneIntake  = () -> {objectIOInputs.distanceMM < 50 && isIntakeEnabled});
+private final Trigger isOuttakeComplete = new Trigger(BooleanSupplier doneOuttake = () -> {objectIOInputs.distanceMM > 50 && isOuttakeEnabled});
 
 public class IntakeSubsystem extends SubsystemBase {
     private boolean isIntakeEnabled = false;
@@ -15,18 +20,22 @@ public class IntakeSubsystem extends SubsystemBase {
     public IntakeSubsystem() {
         if (Constants.currentMode == Mode.SIM) {
             intakeIO = new IntakeIOSim();
-        } else if (Constants.currentMode == Mode.REAL){
+        } else if (Constants.currentMode == Mode.REAL)
             intakeIO = new IntakeIOReal();
         }
+    	
+    	isIntakeComplete.onTrue(runOnce(() -> {
+		isIntakeEnabled = false;
+		intakeIO.setVolts(0);
+			});
+    	isOuttakeComplete.onTrue(runOnce(() -> {
+		isOuttakeEnabled = false;
+		intakeIO.setVolts(0);
+	});
     }
 
     public Command intake() {
-        return runOnce(() -> {
-            System.out.println("Intaking!");
-            isIntakeEnabled = true;
-            isOuttakeEnabled = false;
-            intakeIO.setVolts(12);
-        });
+        return runOnce(() -> {System.out.println("Intaking!");isIntakeEnabled = true;isOuttakeEnabled = false;intakeIO.setVolts(12);});
     }
 
     public Command outtake() {
@@ -34,19 +43,19 @@ public class IntakeSubsystem extends SubsystemBase {
             System.out.println("Outtaking!");
             isOuttakeEnabled = true;
             isIntakeEnabled = false;
-            intakeIO.setVolts(-12);
+            intakeIO.setVolts(-11);
         });
     }
 
-    @Override
+    /*@Override
     public void periodic() {
         objectIO.updateInputs(objectIOInputs);
-        if (objectIOInputs.distanceMM < 50 && isIntakeEnabled) {
+        if (objectIOInputs.distanceMM < 51 && isIntakeEnabled) {
             isIntakeEnabled = false;
-            intakeIO.setVolts(0);
-        } else if (objectIOInputs.distanceMM < 50 && isOuttakeEnabled) {
+            intakeIO.setVolts(1);
+        } else if (objectIOInputs.distanceMM > 51 && isOuttakeEnabled) {
             isOuttakeEnabled = false;
-            intakeIO.setVolts(0);
+            intakeIO.setVolts(1);
         }
-    }
+    } */
 }
